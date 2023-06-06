54cdab49056b5887b5657d7c1609ae690d2e975f
Nasreen <shaikhnasrin88@gmail.com>
2023-06-06 11:06:04 +0530

    All users list

--- src/app/all-users/all-users.component.ts
+++ src/app/all-users/all-users.component.ts
@@ -32,36 +32,22 @@ export class AllUsersComponent implements OnInit {
   currentPage = 1;
   itemsPerPage = 5;
 
-
-  // pageSize = 10; // Number of users to display per page
-
-
-
   constructor(private userService: UserService,
-     private modalService: BsModalService,
-     private router: Router,
-     private paginationConfig: PaginationConfig,
-     private formBuilder: FormBuilder) {
+    private modalService: BsModalService,
+    private router: Router,
+    private paginationConfig: PaginationConfig,
+    private formBuilder: FormBuilder) {
 
-    // this.totalItems = this.users.length;
-  //  this.paginationConfig.pageSize = this.pageSize;
 
-  this.paginationForm = this.formBuilder.group({
-    pageSize: [5], // Default page size
-    // currentPage: [1] // Default current page
-  });
+    this.paginationForm = this.formBuilder.group({
+      pageSize: [5], // Default page size
+    });
 
     this.searchForm = new FormGroup({
       searchQuery: new FormControl('')
     });
-   }
+  }
 
-  //  pageChanged(event: any): void {
-  //   this.currentPage = event.page;
-  // }
-  
-  
-  
   ngOnInit() {
     this.userService.getUsers().subscribe(users => {
       this.users = users;
@@ -74,35 +60,19 @@ export class AllUsersComponent implements OnInit {
       this.search();
     });
 
-    // this.applyPagination();
-    
+
   }
   paginateUsers(): void {
     const startIndex = (this.currentPage - 1) * this.getPageSize();
     const endIndex = startIndex + this.getPageSize();
     this.paginatedUsers = this.users.slice(startIndex, endIndex);
-    // this.paginatedUsers = this.users.slice((this.currentPage - 1) * this.itemsPerPage, this.currentPage * this.itemsPerPage);
   }
 
-  // applyPagination(): void {
-  //   const pageSize = this.paginationForm.get('pageSize')?.value;
-  //   const startIndex = (this.currentPage - 1) * pageSize;
-  //   const endIndex = startIndex + pageSize;
-  
-  //   this.paginatedUsers = this.users.slice(startIndex, endIndex);
-  // }
-  
-  // pageChanged(event: any): void {
-  //   this.paginationForm.patchValue({
-  //     currentPage: event.page
-  //   });
-  //   this.paginateUsers();
-  // }
   pageChanged(event: any): void {
     this.currentPage = event.page;
     this.paginateUsers();
   }
-  
+
 
   getPageSize(): number {
     return this.paginationForm.value.pageSize;
@@ -116,11 +86,6 @@ export class AllUsersComponent implements OnInit {
     this.paginateUsers();
   }
 
-  // onPageSizeChange(): void {
-  //   this.itemsPerPage = this.paginationForm.value.pageSize;
-  //   this.paginateUsers();
-  // }
-  
   search(): void {
     const searchTerm = this.searchForm.get('searchQuery')?.value.toLowerCase();
     this.filteredUsers = this.users.filter(user => {
@@ -129,20 +94,16 @@ export class AllUsersComponent implements OnInit {
       return name.includes(searchTerm) || email.includes(searchTerm);
     });
   }
- 
+
   openModal(template: any, user: User) {
     this.selectedUser = user;
     this.modalRef = this.modalService.show(template);
   }
-  // openModal(template: TemplateRef<any>, user: User) {
-  //   this.selectedUser = user;
-  //   this.modalRef = this.modalService.show(template);
-  // }
+
 
   closeModal() {
     this.modalRef.hide();
   }
-
 
   editUser(user: User) {
     // Redirect to the edit user page with the user ID
@@ -165,12 +126,12 @@ export class AllUsersComponent implements OnInit {
         if (index > -1) {
           this.users.splice(index, 1);
         }
-        // Show success message
+        // Showing success message
         Swal.fire('Deleted!', 'User has been deleted.', 'success');
       }
     });
   }
-  
+
 
 }
 
@@ -195,173 +156,3 @@ export class AllUsersComponent implements OnInit {
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// import { Component, OnInit } from '@angular/core';
-// import { UserService } from '../user.service';
-// import { User } from '../user.interface';
-
-// @Component({
-//   selector: 'app-all-users',
-//   templateUrl: './all-users.component.html',
-//   styleUrls: ['./all-users.component.css']
-// })
-// export class AllUsersComponent {
-
-//   users: User[] = [];
-
-//   constructor(private userService: UserService) {}
-
-//   types: { label: string; value: string }[] = [
-//     { label: 'Admin', value: 'admin' },
-//     { label: 'Manager', value: 'manager' },
-//     { label: 'User', value: 'user' }
-//   ];
-
-//   ngOnInit(): void {
-//     this.getUsers();
-//   }
-
-//   getUsers(): void {
-//     this.userService.getUsers().subscribe(users => {
-//       this.users = users;
-//     });
-//   }
-
-//   deleteUser(userId: number): void {
-//     if (confirm('Are you sure you want to delete this user?')) {
-//       this.userService.deleteUser(userId).subscribe(() => {
-//         this.users = this.users.filter(user => user.id !== userId);
-//       });
-//     }
-//   }
-
-//   // users: User[] =[
-//   //   {
-//   //     name: "john",
-//   //     email: "john@gmail.com",
-//   //     phone: 1223456,
-//   //     experience : 5,
-//   //     type: "Admin"
-//   //   },
-
-//   //   {
-//   //     name: "john",
-//   //     email: "john@gmail.com",
-//   //     phone: 1223456,
-//   //     experience : 5,
-//   //     type: "Manager"
-//   //   },
-//   //   {
-//   //     name: "john",
-//   //     email: "john@gmail.com",
-//   //     phone: 1223456,
-//   //     experience : 5,
-//   //     type: "user"
-//   //   },
-
-//   // ];
-
-//   // types: { label: string; value: string }[] = [
-//   //   { label: 'Admin', value: 'admin' },
-//   //   { label: 'Manager', value: 'manager' },
-//   //   { label: 'User', value: 'user' }
-//   // ];
-
-//   // constructor()
-//   // {
-
-//   // }
-
-//   // viewUser(user :User): void {
-//   //   console.log("User viewed");
-    
-//   // }
-
-//   //  deleteUser(user :User): void {
-
-//   //   console.log("User deleted");
-
-//   // }
-
-// }
-
-
-
-
-
-
-
-
-
-
-  // ngOnInit(): void {
-  //   this.userService.getUsers().subscribe(users => {
-  //     this.users = users;
-  //   });
-  // }
-
-  // ngOnInit() {
-  //     this.userService.getUsers().subscribe(
-  //       (res:any)=>{
-  //         console.log(res);
-  //         this.users = res
-  //       },
-  //       (err:any)=>{
-  //         console.log(err);
-  //       }
-  //     )
-  //   }
-
-
-  // ngOnInit() {
-  //   this.userService.getUsers().subscribe((users: any[]) => {
-  //     this.users = users;
-  //   });
-  // }
-
-//Working code  below
-  // ngOnInit() {
-  //   this.userService.getUsers().subscribe(
-  //     (res:any)=>{
-  //       console.log(res);
-  //       this.users = res
-  //     },
-  //     (err:any)=>{
-  //       console.log(err);
-  //     }
-  //   )
-  // }
-
-  // editUser(id:number  )
-  // {
-
-  // }
-  // deleteUser(id: number) {
-  //   this.userService.deleteUser(id).subscribe(() => {
-  //     this.users = this.users.filter(user => user.id !== id);
-  //   });
-  // }
